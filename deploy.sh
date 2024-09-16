6cb4a2ec3a0b41f6349e5272e27604ca5fbf7769
Sirojiddin <siroj.origin.profile>
2024-09-16 20:08:14 +0900

    feat: develop deploy shell script

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+
+#IF YOU WANT TO USE ONE OF THEM (production), COMMENT OTHER (development).
+
+# PRODUCTION
+git reset --hard
+git pull origin master  
+npm i
+pm2 start ecosystem.config.js --env production
+
+# DEVELOPMENT
+# pm2 start ecosystem.config.js --env development
+
