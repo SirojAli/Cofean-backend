44d13c7f719b9eb43c5385d3b34c6b789151fd30
Sirojiddin <siroj.origin.profile>
2024-09-20 20:22:27 +0900

    fix: modified deploy.sh file

--- deploy.sh
+++ deploy.sh
@@ -3,8 +3,9 @@
 #IF YOU WANT TO USE ONE OF THEM (production), COMMENT OTHER (development).
 
 # PRODUCTION
+git checkout master
 git reset --hard
-git pull origin master  
+git pull origin master
 npm i
 pm2 start ecosystem.config.js --env production
 
